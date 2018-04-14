df3fe274a386e44166fe9fc3d4c580239ded0b31
afeerarc <afeera.rc@gmail.com>
2018-04-14 07:22:38 +0000

    saved

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -1,5 +1,6 @@
 package services;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import data.FeedResponse;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -11,5 +12,25 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query) {
+        FeedResponse feedResponseObject = new FeedResponse();
+        try {
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q", query)
+                    .setQueryParameter("output", "rss")
+                    .get();
 
+            Document feedresponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+        Node item=feedresponse.getFirstChild().getFirstChild().getChildNodes().item(10);
+feedResponseObject.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponseObject.pubDate=item.getChildNodes().item(1).getFirstChild().getNodeValue();
+            feedResponseObject.description=item.getChildNodes().item(2).getFirstChild().getNodeValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
+        }
+        return feedResponseObject;
+    }
 }
