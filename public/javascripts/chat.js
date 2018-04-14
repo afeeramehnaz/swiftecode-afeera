3844eec043fa1a87781a2317b8bdd6f73e4e132d
afeerarc <afeera.rc@gmail.com>
2018-04-14 12:06:36 +0000

    save

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,9 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatcontroller', function ($scope) {
-    $scope.array = [
+    $scope.messages = [
         {
-            'text': 'afeera',
-            'flag': 'true'
-    }
-    ];
+            'sender': 'USER',
+            'text': 'hello'
+          },
+        {
+            'sender': 'BOT',
+            'text': 'hi what can i do for you'
+          },
+        {
+            'sender': 'USER',
+            'text': 'hello babe'
+          },
+        {
+            'sender': 'BOT',
+            'text': 'helllllo'
+          }
+       ];
 });
