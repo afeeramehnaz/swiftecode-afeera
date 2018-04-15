41fb1d245254333d40371ddcd65b75912737e10d
afeerarc <afeera.rc@gmail.com>
2018-04-15 06:01:47 +0000

    save

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -1,17 +1,53 @@
 package actors;
 
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.FeedResponse;
+import data.Message;
+import data.NewsAgentResponse;
+import services.FeedService;
+import services.NewsAgentService;
 
-
+import java.util.UUID;
 
 
 public class MessageActor extends UntypedActor {
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
 
+    private final ActorRef out;
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    private NewsAgentService newsAgentService = new NewsAgentService();
+    private FeedService feedService = new FeedService();
+    private NewsAgentResponse newsAgentResponse=new NewsAgentResponse();
+    private FeedResponse feedResponse=new FeedResponse();
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        if (message instanceof String) {
+            Message messageObject = new Message();
+            messageObject.text=(String)message;
+            messageObject.sender=Message.Sender.USER;
+            out.tell(mapper.writeValueAsString(messageObject),self());
 
+            String query=newsAgentService.getNewsAgentResponse("Find"+message,UUID.randomUUID()).query;
+            feedResponse=feedService.getFeedByQuery(newsAgentResponse.query);
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse= feedResponse;
+            messageObject.sender=Message.Sender.BOT;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+        }
 
     }
+
+
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -1,5 +1,14 @@
 package data;
 
+
+
+
 public class Message {
+    public String text;
+    public FeedResponse feedResponse;
+    public Sender sender;
+    public enum Sender {
+        BOT, USER
+    }
 
 }
